0a3d72cdd206fed0a55016a6500afe2b570dd3b2
Diocleniax789 <julioramdom123@gmail.com>
2024-11-18 02:19:07 -0300

    se agregaron las librerias y los prototipos

--- sistema-tickets.c
+++ sistema-tickets.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+struct tickCol{
+    char incid[200];
+    char modulo[15];
+};
+
+void carga_ticket(struct tickCol*,int*);
+char * identifica_modulo(char*);
+char * mayor_modulo(struct tickCol*,int,int*);
 
 int main(){
 
@@ -7,5 +18,5 @@ int main(){
 
 
 
-
+    return 0;
 }
