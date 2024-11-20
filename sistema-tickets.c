f2e868aae4cc5de288628f594b7a5a82894cfa54
Diocleniax789 <julioramdom123@gmail.com>
2024-11-19 23:30:13 -0300

    se creo el programa principal, osea el main

--- sistema-tickets.c
+++ sistema-tickets.c
@@ -12,11 +12,30 @@ char * identifica_modulo(char*);
 char * mayor_modulo(struct tickCol*,int,int*);
 
 int main(){
+    struct tickCol tickets[200];
+    char op, *mayor_mod;
+    int tickets_cargados = 0, ocurrencias_del_modulo = 0;
 
+    do{
 
+        carga_ticket(tickets,&tickets_cargados);
 
+        do{
+            fflush(stdin);
+            printf("\n - Agregar otro ticket? Ingrese <s/n> : ");
+            scanf("%c",&op);
+            if(op != 's' && op != 'n'){
+                printf("\n x ERROR. INGRESE 's' O 'n' x \n");
+            }
+        } while(op != 's' && op != 'n');
 
+    } while(op != 'n');
 
+    mayor_mod = mayor_modulo(tickets,tickets_cargados,&ocurrencias_del_modulo);
+
+    printf("\n");
+    printf("\n ----------------------------------------------------------------");
+    printf("\n El modulo de mayor ocurrencia es: %s y tiene %i incidencias",mayor_mod,ocurrencias_del_modulo);
 
     return 0;
 }
